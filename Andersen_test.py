e87b007a40c99e127417086d5e2bb885217efacb
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-01-25 21:43:54 +0300

    Созданы файлы ответов.

---
+++ Andersen_test.py
@@ -0,0 +1,17 @@
+print("Введите число:")
+n = int(input())
+if n > 7:
+    print("Привет!")
+
+print("Введите имя:")
+name = input()
+if name == "Вячеслав":
+    print("Привет, " + name)
+else: print("Нет такого имени")
+
+print("Введите числа через пробел:")
+l = [int(i) for i in input().split()]
+for i in range(len(l)-1, -1, -1):
+    if l[i] % 3 != 0 or l[i] == 0:
+        l.remove(l[i])
+print(l)
